e31feef29c8b5290deb40fecdc5273aa75e07505
Mu-selim <muhamadselim70@gmail.com>
2024-04-18 13:29:49 +0200

    Add clerk authentication

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,3 +1,8 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <p className="text-3xl font-bold text-indigo-500">Hello Discord clone</p>;
+  return <div className="p-4 flex items-center justify-center gap-4">
+    <div className="rounded-full p-1 shadow-2xl shadow-slate-950"><UserButton afterSignOutUrl="/" /></div>
+    <p className="text-3xl font-bold text-indigo-500">Hello Discord clone</p>
+  </div>
 }
