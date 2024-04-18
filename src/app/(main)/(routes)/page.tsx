cd51757c81f5877f1efd53608de609b764bf6cdd
Muhammad Selim <muhamadselim70@gmail.com>
2024-04-18 13:52:08 +0200

    Revert "3 add dark light modes"

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,14 +1,3 @@
-import { ModeToggle } from "@/components/modeToggle";
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div className="flex items-center justify-center gap-4 p-4">
-      <div className="rounded-full p-1 shadow-2xl shadow-slate-950">
-        <UserButton afterSignOutUrl="/" />
-      </div>
-      <p className="text-3xl font-bold text-indigo-500">Hello Discord clone</p>
-      <ModeToggle />
-    </div>
-  );
+  return <p className="text-3xl font-bold text-indigo-500">Hello Discord clone</p>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/providers/theme-provider";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
@@ -17,14 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body className={font.className}>
-          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="discord-clone-theme">
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
   );
 }
