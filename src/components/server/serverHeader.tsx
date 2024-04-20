af78774422d98309ea396fcc33468659b486de3b
Mu-selim <muhamadselim70@gmail.com>
2024-04-20 15:30:56 +0200

    Add channels of a specific server sidebar

--- src/components/server/serverHeader.tsx
+++ src/components/server/serverHeader.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ChevronDown, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
 import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
 
 type ServerHeaderProps = {
@@ -31,34 +31,40 @@ export function ServerHeader({ server, role }: ServerHeaderProps) {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 space-y-0.5 text-xs font-medium text-black dark:text-neutral-400">
         {isOwner && (
-          <DropdownMenuItem className="flex cursor-pointer items-center px-3 py-2 text-sm text-indigo-600 dark:text-indigo-400">
+          <DropdownMenuItem className="flex cursor-pointer items-center px-3 py-2 text-sm text-indigo-600 transition hover:outline-none dark:text-indigo-400 hover:dark:text-white">
             <span>Invite People</span>
             <UserPlus className="ml-auto size-4" />
           </DropdownMenuItem>
         )}
         {isOwner && (
-          <DropdownMenuItem className="flex cursor-pointer items-center px-3 py-2 text-sm">
+          <DropdownMenuItem className="flex cursor-pointer items-center px-3 py-2 text-sm transition hover:outline-none hover:dark:text-white">
             <span>Server Settings</span>
             <Settings className="ml-auto size-4" />
           </DropdownMenuItem>
         )}
         {isOwner && (
-          <DropdownMenuItem className="flex cursor-pointer items-center px-3 py-2 text-sm">
+          <DropdownMenuItem className="flex cursor-pointer items-center px-3 py-2 text-sm transition hover:outline-none hover:dark:text-white">
             <span>Manage Members</span>
             <Users className="ml-auto size-4" />
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="flex cursor-pointer items-center px-3 py-2 text-sm">
+          <DropdownMenuItem className="flex cursor-pointer items-center px-3 py-2 text-sm transition hover:outline-none hover:dark:text-white">
             <span>Create Channel</span>
             <PlusCircle className="ml-auto size-4" />
           </DropdownMenuItem>
         )}
         {isAdmin && <DropdownMenuSeparator />}
         {isOwner && (
-          <DropdownMenuItem className="flex cursor-pointer items-center px-3 py-2 text-sm">
+          <DropdownMenuItem className="flex cursor-pointer items-center px-3 py-2 text-sm text-rose-500 transition hover:outline-none hover:dark:text-white">
             <span>Delete Server</span>
             <Trash className="ml-auto size-4" />
+          </DropdownMenuItem>
+        )}
+        {!isOwner && (
+          <DropdownMenuItem className="flex cursor-pointer items-center px-3 py-2 text-sm text-rose-500 transition hover:outline-none hover:dark:text-white">
+            <span>Leave Server</span>
+            <LogOut className="ml-auto size-4" />
           </DropdownMenuItem>
         )}
       </DropdownMenuContent>
