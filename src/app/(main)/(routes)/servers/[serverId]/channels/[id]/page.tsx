e692551e2f189f8903a257799969203ebea9fcef
Mu-selim <muhamadselim70@gmail.com>
2024-05-01 10:50:39 +0300

    Added Chat Input component

--- src/app/(main)/(routes)/servers/[serverId]/channels/[id]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[id]/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { currentProfile } from "@/lib/currentProfile";
 import { db } from "@/lib/db";
 import { ChatHeader } from "@/components/chat/chatHeader";
+import { ChatInput } from "@/components/chat/chatInput";
 
 type ChannelIdPageProps = {
   params: {
@@ -26,8 +27,18 @@ export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
   if (!channel || !member) return redirect("/");
 
   return (
-    <div className="h-full w-full bg-white dark:bg-[#313338]">
+    <div className="h-full w-full bg-white dark:bg-[#313338] flex flex-col">
       <ChatHeader serverId={channel.serverId} name={channel.name} type="channel" />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        apiURL="/api/socket/messages"
+        name={channel.name}
+        type="channel"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      />
     </div>
   );
 }
